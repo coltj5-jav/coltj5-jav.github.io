3201f47e9dd316a86e3a9eee8937bd0a0d6c475d
Colt Feltes <coltfeltes@Colts-MacBook-Pro-2.local>
2025-12-16 11:39:23 -0700

    Quartz sync: Dec 16, 2025, 11:39 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,10 +4,11 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: header: [
+  header: [
   Component.Flex({
     components: [
-      { Component: Component.Search(), grow: true },
+      { Component: Component.PageTitle() },          // adds “Colt Feltes” to the top bar
+      { Component: Component.Search(), grow: true }, // search stretches
       { Component: Component.Darkmode() },
       { Component: Component.ReaderMode() },
     ],
@@ -34,7 +35,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
     // Component.Explorer(),
   ],
   right: [
